770f0dd670138e7728d65e74398febf0fee174db
Jan Wedekind <jan.wedekind@roke.co.uk>
2017-10-03 16:00:49 +0100

    Data set

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 * [Theano](http://deeplearning.net/software/theano/)
 * [Deeplearning tutorial](http://deeplearning.net/tutorial/contents.html)
 * [Brachmann - Learning 6D Object Pose Estimation using 3D Object Coordinates](http://wwwpub.zih.tu-dresden.de/~cvweb/publications/papers/2014/PoseEstimationECCV2014.pdf)
+* [Andrew Ng - Lecture 1.2 - Introduction Supervised Learning](https://www.youtube.com/watch?v=bQI5uDxrFfA)
--- data.py
+++ data.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python3
+import pytest
 import numpy as np
 import gzip
 import pickle
 
-# https://stackoverflow.com/questions/11305790/pickle-incompatability-of-numpy-arrays-between-python-2-and-3
-train, valid, test = pickle.load(gzip.open('mnist.pkl.gz', 'rb'), encoding='iso-8859-1')
+
+if __name__ == '__main__':
+    # https://stackoverflow.com/questions/11305790/pickle-incompatability-of-numpy-arrays-between-python-2-and-3
+    training, validation, testing = pickle.load(gzip.open('mnist.pkl.gz', 'rb'), encoding='iso-8859-1')
