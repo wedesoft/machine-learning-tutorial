f57d23b9426e949e438a699749b224df9b7db692
Jan Wedekind <jan.wedekind@roke.co.uk>
2017-10-04 11:52:25 +0100

    Use subset of training data

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # External Links
 
-* [Theano](http://deeplearning.net/software/theano/)
 * [MNIST challenge](http://yann.lecun.com/exdb/mnist/)
+* [Theano](http://deeplearning.net/software/theano/)
 * [Deeplearning tutorial](http://deeplearning.net/tutorial/contents.html)
 * [Brachmann - Learning 6D Object Pose Estimation using 3D Object Coordinates](http://wwwpub.zih.tu-dresden.de/~cvweb/publications/papers/2014/PoseEstimationECCV2014.pdf)
 * [Andrew Ng - Lecture 1.2 - Introduction Supervised Learning](https://www.youtube.com/watch?v=bQI5uDxrFfA)
--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import sys
 import pytest
 import gzip
 import pickle
@@ -80,17 +81,70 @@ class TestCorrelationCoefficient:
         assert_array_equal(correlation_coefficient([[-1, 1], [1, -1], [-1, 1]], [-1, 1]), [1, -1, 1])
 
 
+def random_choice(count, size):
+    return np.random.choice(count, size, replace=False)
+
+
+class TestRandomChoice:
+    def test_select_one_of_one(self):
+        assert_array_equal(random_choice(1, 1), [0])
+
+    def test_select_one_of_ten(self):
+        result = random_choice(10, 1)
+        assert result[0] >= 0 and result[0] < 10
+
+    def test_select_distinct_values(self):
+        assert sorted(random_choice(10, 10)) == list(range(10))
+
+
+def random_selection(size, *arrays):
+    indices = random_choice(len(arrays[0]), size)
+    result = tuple(np.take(array, indices, axis=0) for array in arrays)
+    return result[0] if len(result) == 1 else result
+
+
+class TestRandomSelection:
+    @pytest.fixture
+    def choose_first(self, monkeypatch):
+        monkeypatch.setattr(sys.modules[__name__], 'random_choice', lambda count, size: range(size))
+
+    @pytest.fixture
+    def choose_last(self, monkeypatch):
+        monkeypatch.setattr(sys.modules[__name__], 'random_choice', lambda count, size: range(count - size, count))
+
+    def test_select_one(self):
+        assert_array_equal(random_selection(1, [42]), [42])
+
+    def test_select_first_of_two(self, choose_first):
+        assert_array_equal(random_selection(1, [3, 5]), [3])
+
+    def test_select_second_of_two(self, choose_last):
+        assert_array_equal(random_selection(1, [3, 5]), [5])
+
+    def test_select_from_multiple_arrays(self, choose_first):
+        result = random_selection(1, [1, 2, 3], [4, 5, 6])
+        assert_array_equal(result[0], [1])
+        assert_array_equal(result[1], [4])
+
+    def test_select_from_2d_array(self):
+        assert_array_equal(random_selection(1, [[1, 2, 3]]), [[1, 2, 3]])
+
+
 def show(img, wait=True):
     cv2.imshow('show', cv2.resize(img.reshape(28, 28), (280, 280)))
     return cv2.waitKey(-1 if wait else 1) != 27
 
 
 def save(file_name, img):
-    cv2.imwrite(file_name, img.reshape(28, 28))
+    cv2.imwrite(file_name, (img.reshape(28, 28) * 255).astype(np.uint8))
 
 
 def index(data, value):
     return np.where(data == value)[0][0]
+
+
+def make_classifier(images, labels):
+    return lambda test: labels[np.argmax(correlation_coefficient(images, test))]
 
 
 # camera -> connected components -> label
@@ -103,11 +157,12 @@ if __name__ == '__main__':
     print(len(testing[1]), 'testing samples')
     n = 0
     error = 0
+    classifier = make_classifier(*random_selection(5000, *training))
     for test, label in zip(testing[0], testing[1]):
-        result = training[1][np.argmax(correlation_coefficient(training[0], test))]
+        result = classifier(test)
         print("%d (%d)" % (result, label))
         if result != label:
-            save('%03d_%d.png' % (n, result), test)
+            save('%05d_%d.png' % (n, result), test)
             error += 1
         n += 1
         print('error rate %5.2f%%' % (100 * error / n))
