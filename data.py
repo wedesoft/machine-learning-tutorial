68395e45fb2f4ce43ed800fc15ff3d5b88a40449
Jan Wedekind <jan.wedekind@roke.co.uk>
2017-10-04 10:05:18 +0100

    Recognise characters using cross-correlation

--- data.py
+++ data.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python3
 import pytest
-import numpy as np
 import gzip
 import pickle
+import numpy as np
+from numpy.testing import assert_array_equal
+import cv2
 
 
 def average(data):
-    return np.average(data)
+    return np.average(data, axis=-1)
 
 
 class TestAverage:
     def test_single_element(self):
         assert average([42]) == 42
 
-    def tesT_two_elements(self):
+    def test_two_elements(self):
         assert average([10, 20]) == 15
+
+    def test_batch(self):
+        assert_array_equal(average([[5], [7]]), [5, 7])
+
+
+def deviation(data):
+    return np.transpose(np.transpose(data) - average(data))
 
 
 def std(data):
-    deviation = data - average(data)
-    return average(deviation ** 2) ** 0.5
+    return average(deviation(data) ** 2) ** 0.5
 
 
 class TestStandardDeviation:
@@ -29,9 +37,12 @@ class TestStandardDeviation:
     def test_two_elements(self):
         assert std([10, 20]) == 5
 
+    def test_batch(self):
+        assert_array_equal(std([[3], [5]]), [0, 0])
+
 
 def covariance(x, y):
-    return average((x - average(x)) * (y - average(y)))
+    return average(deviation(x) * deviation(y))
 
 
 class TestCovariance:
@@ -43,6 +54,9 @@ class TestCovariance:
 
     def test_negative(self):
         assert covariance([-1, 1], [1, -1]) == -1
+
+    def test_batch(self):
+        assert_array_equal(covariance([[-1, 1], [-1, 1], [1, -1]], [-1, 1]), [1, 1, -1])
 
 
 def correlation_coefficient(x, y):
@@ -62,6 +76,18 @@ class TestCorrelationCoefficient:
     def test_normalise_both(self):
         assert correlation_coefficient([-2, 2], [-4, 4]) == 1
 
+    def test_batch(self):
+        assert_array_equal(correlation_coefficient([[-1, 1], [1, -1], [-1, 1]], [-1, 1]), [1, -1, 1])
+
+
+def show(img, wait=True):
+    cv2.imshow('show', cv2.resize(img.reshape(28, 28), (280, 280)))
+    return cv2.waitKey(-1 if wait else 1) != 27
+
+
+def index(data, value):
+    return np.where(data == value)[0][0]
+
 
 # camera -> connected components -> label
 
@@ -71,3 +97,16 @@ if __name__ == '__main__':
     print(len(training[1]), 'training samples')
     print(len(validation[1]), 'validation samples')
     print(len(testing[1]), 'testing samples')
+    n = 0
+    ok = 0
+    for test, label in zip(testing[0], testing[1]):
+        result = training[1][np.argmax(correlation_coefficient(training[0], test))]
+        print("%d (%d)" % (result, label))
+        if result != label:
+            cv2.imwrite('%03d_%d.png' % (n, result), test)
+        else:
+            ok += 1
+        n += 1
+        print('error rate %5.3f' % (ok / n))
+        if not show(test, False):
+            break
