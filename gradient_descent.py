3d0cf63e1f08c428c03e449e081bb603ec4e3c21
Jan Wedekind <jan.wedekind@roke.co.uk>
2017-10-05 16:36:46 +0100

    Plot samples

---
+++ gradient_descent.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib
+matplotlib.use('QT4Agg')
+import matplotlib.pyplot as plt
+
+
+t0 = 0.7
+t1 = 0.3
+noise = 0.2
+
+
+if __name__ == '__main__':
+    x = np.random.rand(100)
+    error = np.random.rand(100) * noise
+    y = t0 + t1 * x + error
+    plt.plot(x, y, 'o')
+    plt.show()
