1bd9b5e5c0ef054c377a5f75614f71636cc67343
Jan Wedekind <jan.wedekind@roke.co.uk>
2017-10-06 13:43:53 +0100

    Show legend in plots

--- gradient_descent.py
+++ gradient_descent.py
@@ -22,18 +22,22 @@ def gradient_function():
 if __name__ == '__main__':
     x = np.random.rand(100)
     error = np.random.rand(100) * 0.2 - 0.1
-    y = 0.7 + 0.3 * x + error
-    plt.plot(x, y, 'o')
+    y = 0.2 + 0.5 * x + error
+    plt.plot(x, y, 'o', label='data')
 
     alpha = 0.5
     gradient = gradient_function()
     t0, t1 = 0.0, 0.0
 
-    for c in np.arange(0.1, 1, 0.1):
+    for i in range(10):
         dt0, dt1 = gradient(x, y, t0, t1)
         t0 -= alpha * dt0
         t1 -= alpha * dt1
-        plt.plot(np.arange(2), t0 + np.arange(2) * t1, color=(0, 0.5, 0, c))
-        print(t0, t1)
+        plt.plot(np.arange(2), t0 + np.arange(2) * t1, color=(0, 0.5, 0, i * 0.1 + 0.1), label="iteration %d" % i)
 
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Gradient Descent')
+    plt.legend()
+    plt.axis([0, 1, 0, 1])
     plt.savefig('gradient_descent.pdf')
--- least_squares.py
+++ least_squares.py
@@ -9,10 +9,16 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     x = np.random.rand(100)
     error = np.random.rand(100) * 0.2 - 0.1
-    y = 0.7 + 0.3 * x + error
-    plt.plot(x, y, 'o')
+    y = 0.2 + 0.5 * x + error
+
+    plt.plot(x, y, 'o', label='data')
 
     t0, t1 = np.dot(np.linalg.inv([[len(x), np.sum(x)], [np.sum(x), np.sum(x*x)]]), [np.sum(y), np.sum(x * y)])
-    plt.plot(np.arange(2), t0 + np.arange(2) * t1)
+    plt.plot(np.arange(2), t0 + np.arange(2) * t1, label='result')
 
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Least Squares Estimate')
+    plt.legend()
+    plt.axis([0, 1, 0, 1])
     plt.savefig('least_squares.pdf')
