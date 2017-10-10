c46b07764c910a929267036a1e591e0774e66c2e
Jan Wedekind <jan.wedekind@roke.co.uk>
2017-10-10 16:25:45 +0100

    working on cost function

--- backprop.py
+++ backprop.py
@@ -15,9 +15,9 @@ class Layer:
         self.bias_ = theano.shared(value=np.array(bias), name='bias')
         self.g_ = self.sigmoid_function if activation is None else activation
         x_ = T.vector('x')
-        self.fun = theano.function([x_], outputs=self.output(x_))
+        self.fun = theano.function([x_], outputs=self.output_function(x_))
 
-    def output(self, x_):
+    def output_function(self, x_):
         z_ = T.dot(self.weight_, x_) + self.bias_
         a_ = self.g_(z_)
         return a_
@@ -61,17 +61,20 @@ class MLP:
     def __init__(self, weights, biases):
         self.layers = [Layer(weight, bias) for weight, bias in zip(weights, biases)]
         x_ = T.vector('x')
-        self.fun = theano.function([x_], outputs=self.output(x_))
+        self.fun = theano.function([x_], outputs=self.output_function(x_))
 
-    def output(self, x_):
+    def output_function(self, x_):
         a_ = x_
         for layer in self.layers:
-            a_ = layer.output(a_)
+            a_ = layer.output_function(a_)
         return a_
 
     @staticmethod
     def logistic_cost(h, y):
         return -(math.log(h) * y + (1 - y) * math.log(1 - h))
+
+    def cost(self, x, y):
+        return self.logistic_cost(self(x[0])[0], y[0][0])
 
     def __call__(self, x):
         return self.fun(x)
@@ -86,17 +89,29 @@ class TestMLP:
     def layer2(self):
         return Layer([[2]], [1])
 
-    def test_single_layer(self, layer1):
-        assert_array_equal(MLP([[[2, 3, 5]]], [[5]])([1, 2, -3]), layer1([1, 2, -3]))
+    @pytest.fixture
+    def mlp1(self):
+        return MLP([[[2, 3, 5]]], [[5]])
 
-    def test_two_layers(self, layer1, layer2):
-        assert_array_equal(MLP([[[2, 3, 5]], [[2]]], [[5], [1]])([1, 2, -3]), layer2(layer1([1, 2, -3])))
+    @pytest.fixture
+    def mlp2(self):
+        return MLP([[[2, 3, 5]], [[2]]], [[5], [1]])
+
+    def test_single_layer(self, mlp1, layer1):
+        assert_array_equal(mlp1([1, 2, -3]), layer1([1, 2, -3]))
+
+    def test_two_layers(self, mlp2, layer1, layer2):
+        assert_array_equal(mlp2([1, 2, -3]), layer2(layer1([1, 2, -3])))
 
     def test_logistic_cost(self):
         assert MLP.logistic_cost(0.1, 0) > 0
         assert MLP.logistic_cost(0.1, 0) < 0.2
         assert MLP.logistic_cost(0.9, 1) == MLP.logistic_cost(0.1, 0)
         assert MLP.logistic_cost(0.9, 0) > 2
+
+    def test_cost(self, mlp2):
+        result = mlp2([1, 2, -3])
+        assert mlp2.cost([[1, 2, -3]], [[1]]) == MLP.logistic_cost(result, 1)
 
 
 if __name__ == '__main__':
